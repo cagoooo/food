286ef7d20975fd04d40f0b37eb9cb5212d711161
cagoooo <131627413+cagoooo@users.noreply.github.com>
2026-09-09 07:11:25 +0800

    fix: 更新阿凱老師教師頁連結

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -73,7 +73,7 @@ const LandingPage = ({ onSelectMode }) => {
                 transition={{ delay: 1, duration: 1 }}
             >
                 <a
-                    href="https://www.smes.tyc.edu.tw/modules/tadnews/page.php?ncsn=11&nsn=16#a5"
+                    href="https://www.smes.tyc.edu.tw/modules/school/index.php?department_id=2&zone_id=0&page_id=2&content_id=11&type=news&from_op=all_news#a5"
                     target="_blank"
                     rel="noopener noreferrer"
                     style={{ color: 'inherit', textDecoration: 'none' }}
